a1a7a3cf6ff3b8489222e5730c9f19d661f4974c
SanchesVitor <vitorsanches06@hotmail.com>
2023-08-05 16:18:59 -0300

    Create habits list and Implementing the logic of showing the last 7 days

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+import DayState from "@/components/DayState";
+import Image from "next/image";
 
 export default function Home() {
-  const habits = {};
+  const habits = {
+    'beber água': {
+      '2023-07-18': true,
+      '2023-07-17': false,
+      '2023-07-16': false,
+    },
+    'estudar programação': {
+      '2023-07-18': false,
+      '2023-07-17': true,
+      '2023-07-16': true,
+    },
+  };
+
+  const today = new Date();
+  const todayWeekDay = today.getDay();
+  const weekDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
+
+  const sortedWeekDays = weekDays.slice(todayWeekDay + 1).concat(weekDays.slice(0, todayWeekDay + 1));
 
   return (
     <main className="container relative flex flex-col gap-8 px-4 pt-16">
@@ -10,6 +29,38 @@ export default function Home() {
             Você não tem hábitos cadastrados
           </h1>
         ))}
+      {
+        habits !== null && Object.entries(habits).map(
+          ([habit, habitStreak]) => (
+            <div key={habit} className="flex flex-col gap-2">
+              <div className="flex justify-between items-center">
+                <span className="text-xl font-light text-white font-sans">
+                  {habit}
+                </span>
+                <button>
+                  <Image
+                    src="/images/trash.svg"
+                    width={20}
+                    height={20}
+                    alt="Icone de lixeira Vermelha"
+                  />
+                </button>
+              </div>
+              <section className="grid grid-cols-7 bg-neutral-800 rounded-md p-2">
+                {sortedWeekDays.map((day) => (
+                  <div key={day} className="flex flex-col last:font-bold">
+                    <span className="font-sans text-xs text-white text-center">
+                      {day}
+                    </span>
+                    {/*Day-State*/}
+                    <DayState day={true} />
+                  </div>
+                ))}
+              </section>
+            </div>
+          )
+        )
+      }
     </main>
   );
 }
